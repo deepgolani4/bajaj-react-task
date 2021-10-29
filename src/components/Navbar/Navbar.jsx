187adeabdcadb6b8721fb41507012355c79c9492
Abhay-applore <abhay@applore.in>
2021-10-30 00:37:30 +0530

    Navbar active link  animation and visuals👀

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,7 +1,11 @@
 import React from 'react';
 import styles from './navbar.module.scss';
 import { Link } from 'react-router-dom';
+import { useLocation } from 'react-router-dom';
 const Navbar = () => {
+    const location = useLocation();
+    const pathName = location.pathname;
+    console.log(pathName === '/');
     return (
         <div className={styles.navbar}>
             <div className={styles.navbar_wrapper}>
@@ -15,19 +19,44 @@ const Navbar = () => {
                 </div>
                 <div className={styles.nav_middle}>
                     <ul>
-                        <Link to='/' className={styles.nav_links}>
+                        <Link
+                            to='/'
+                            className={
+                                pathName === '/'
+                                    ? `${styles.active_link}`
+                                    : `${styles.nav_links}`
+                            }
+                        >
                             Home
                         </Link>
                         <Link
                             to='/cryptocurrencies'
-                            className={styles.nav_links}
+                            className={
+                                pathName === '/cryptocurrencies'
+                                    ? `${styles.active_link}`
+                                    : `${styles.nav_links}`
+                            }
                         >
                             Cryptocurrencies
                         </Link>
-                        <Link to='/news' className={styles.nav_links}>
+                        <Link
+                            to='/news'
+                            className={
+                                pathName === '/news'
+                                    ? `${styles.active_link}`
+                                    : `${styles.nav_links}`
+                            }
+                        >
                             News
                         </Link>
-                        <Link to='/exchanges' className={styles.nav_links}>
+                        <Link
+                            to='/exchanges'
+                            className={
+                                pathName === '/exchanges'
+                                    ? `${styles.active_link}`
+                                    : `${styles.nav_links}`
+                            }
+                        >
                             Exchanges
                         </Link>
                     </ul>
