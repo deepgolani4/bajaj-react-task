976033efe370030fbbd2925cd5f08e08aa9d67e7
Deep Golani <54791570+deepgolani4@users.noreply.github.com>
2021-11-10 12:28:43 +0530

    fixed minor bug

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -5,55 +5,7 @@ import { styled } from '@mui/material/styles';
 import Box from '@mui/material/Box';
 import Paper from '@mui/material/Paper';
 import Grid from '@mui/material/Grid';
-
-// const Footer = () => {
-//     return (
-//         <Container className={styles.Cont}>
-//         <TableRow>
-//             <TableCell className={styles.photoCell}>
-//                 <img
-//                 height="30px"
-//                 width="30px"
-//                 src='/assets/logo.svg'
-//                 />
-//                 <p  className={styles.font}>Crypto Chain</p>
-    
-//             </TableCell>
-//             <TableCell className={styles.first}> Contribute </TableCell>
-//             <TableCell className={styles.first}>Features </TableCell>
-//             <TableCell className={styles.first}> Contact Us </TableCell>
-//         </TableRow>
-//         <TableRow>
-//             <TableCell className={styles.cell}>Copyright @ 2021</TableCell>
-//             <TableCell className={styles.cell}> Know about developers </TableCell>
-//             <TableCell className={styles.cell}> Currency </TableCell>
-//             <TableCell className={styles.cell}> Mail Us</TableCell>
-//         </TableRow>
-
-//         <TableRow>
-//             <TableCell className={styles.cell}>            </TableCell>
-//             <TableCell className={styles.cell}> join us as a Developer  </TableCell>
-//             <TableCell className={styles.cell}> News </TableCell>
-//             <TableCell className={styles.cell}> Linkedin</TableCell>
-//         </TableRow>
-
-//         <TableRow>
-//             <TableCell className={styles.cell}>            </TableCell>
-//             <TableCell className={styles.cell}>Suggest Us</TableCell>
-//             <TableCell className={styles.cell}> Exchanges </TableCell>
-//             <TableCell className={styles.cell}> Twitter </TableCell>
-//         </TableRow>
-           
-//         <TableRow>
-//             <TableCell className={styles.cell}>            </TableCell>
-//             <TableCell className={styles.cell}></TableCell>
-//             <TableCell className={styles.cell}> NFTs </TableCell>
-//             <TableCell className={styles.cell}>6354861997 </TableCell>
-//         </TableRow>
-//         </Container>
-//     )
-// }
-
+import Link from '@mui/material/Link';
 
 const Item = styled(Paper)(({ theme }) => ({
     ...theme.typography.body2,
@@ -66,20 +18,21 @@ const Item = styled(Paper)(({ theme }) => ({
 const Footer = () => {
 return (
     <>
-    <Grid container spacing={1} columns={15} className={styles.cell}>
-    <Grid item xs={3} className={styles.cell}>
+    <div className={styles.mainBox}>
+    <Grid container spacing={1} columns={14} className={styles.cell}>
+    <Grid item xs={2} className={styles.cell}>
         <item className={styles.cell} >
             <Grid className={styles.photoCell} >
             <img src='/assets/logo.svg' />
             </Grid>
             </item>
     </Grid>
-    <Grid item xs={3} className={styles.logoText}> Crypto Chain </Grid>
+    <Grid item xs={2.5} className={styles.logoText}> Crypto Chain </Grid>
     <Grid item xs={3}>
         <item className={styles.cell}>Contribute</item>
     </Grid>
     <Grid item xs={3}>
-        <item className={styles.cell}>Currency</item>
+        <item className={styles.cell}>Features</item>
     </Grid>
     <Grid item xs={3}>
         <item className={styles.cell}>Contract Us</item>
@@ -88,59 +41,77 @@ return (
 
     {/* // */}
 
-    <Grid container spacing={1} columns={15} className={styles.cell}>
-    <Grid item xs={6} className={styles.cell}>
+    <Grid container spacing={1} columns={14} className={styles.cell}>
+    <Grid item xs={4.5} className={styles.cell}>
    
     </Grid>
     <Grid item xs={3} href="#">
+        <Link href="https://github.com/deepgolani4/bajaj-react-task" underline="none" color="white">
         <item className={styles.first} href="#">Know About Developers</item>
+        </Link>
     </Grid>
     <Grid item xs={3}>
-        <item className={styles.first}>Features</item>
+    <Link href="/cryptocurrencies" underline="none" color="white">
+        <item className={styles.first}>Currencies</item>
+        </Link>
     </Grid>
     <Grid item xs={3}>
+    <Link href="mailto:abc@xyz.com" underline="none" color="white">
         <item className={styles.first}>Mail Us</item>
+        </Link>
     </Grid>
     </Grid>
 
     {/* // */}
 
-    <Grid container spacing={1} columns={15} className={styles.cell}>
-    <Grid item xs={6} className={styles.cell}>
+    <Grid container spacing={1} columns={14} className={styles.cell}>
+    <Grid item xs={4.5} className={styles.cell}>
         
     </Grid>
     <Grid item xs={3}>
+    <Link href="https://github.com/deepgolani4/bajaj-react-task" underline="none" color="white">
         <item className={styles.first}>join us as Developer</item>
+        </Link>
     </Grid>
     <Grid item xs={3}>
+    <Link href="/news" underline="none" color="white">
         <item className={styles.first}>News</item>
+        </Link>
     </Grid>
     <Grid item xs={3}>
+    <Link href="/" underline="none" color="white">
         <item className={styles.first}>LinkedIn</item>
+        </Link>
     </Grid>
     </Grid>
 
     {/* // */}
 
-    <Grid container spacing={1} columns={15} className={styles.cell}>
-    <Grid item xs={6} className={styles.cell}>
+    <Grid container spacing={1} columns={14} className={styles.cell}>
+    <Grid item xs={4.5} className={styles.cell}>
         
     </Grid>
     <Grid item xs={3}>
+    <Link href="https://nz3kqgsyqda.typeform.com/to/Ty8IUsml" underline="none" color="white">
         <item className={styles.first}>Suggest us</item>
+        </Link>
     </Grid>
     <Grid item xs={3}>
+    <Link href="/exchanges" underline="none" color="white">
         <item className={styles.first}>Exchanges</item>
+        </Link>
     </Grid>
     <Grid item xs={3}>
+    <Link href="https://twitter.com/deepgolani4" underline="none" color="white">
         <item className={styles.first}>Twitter</item>
+        </Link>
     </Grid>
     </Grid>
 
     {/* // */}
 
-    <Grid container spacing={1} columns={15} className={styles.cell}>
-    <Grid item xs={6} className={styles.cell}>
+    <Grid container spacing={1} columns={14} className={styles.cell}>
+    <Grid item xs={4.5} className={styles.cell}>
         
     </Grid>
     <Grid item xs={3}>
@@ -153,7 +124,7 @@ return (
         <item className={styles.first}>6354861997</item>
     </Grid>
     </Grid>
-
+    </div>
    
     </>
     
