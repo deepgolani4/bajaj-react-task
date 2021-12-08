71b7362e4af2a51e7f9f416e85a7732c5f688de5
Deep Agrawal <deepsanjay.agrawal2018@vitstudent.ac.in>
2021-12-08 14:23:53 +0530

    feat: loading animation

--- src/components/News/News.jsx
+++ src/components/News/News.jsx
@@ -1,44 +1,47 @@
-import { useState, useEffect } from 'react';
-import { NewsCard } from '..';
-import styles from './News.module.scss';
-import { useGetCryptosQuery } from '../../services/cryptoApi';
-import { useGetCryptoNewsQuery } from '../../services/cryptoNewsApi';
-import Dropdown from '../Reusable/Dropdown/Dropdown';
+import { useState, useEffect } from 'react'
+import { NewsCard } from '..'
+import styles from './News.module.scss'
+import { useGetCryptosQuery } from '../../services/cryptoApi'
+import { useGetCryptoNewsQuery } from '../../services/cryptoNewsApi'
+import Dropdown from '../Reusable/Dropdown/Dropdown'
+import LoadingCube from '../LoadingCube/LoadingCube'
+
 const News = ({ simplified }) => {
-    const [newsCategory, setNewsCategory] = useState('Cryptocurrency');
-    const { data: cryptoNews } = useGetCryptoNewsQuery({
-        newsCategory,
-        count: simplified ? 3 : 20,
-    });
-    console.log(cryptoNews);
-    let { data: cryptosList } = useGetCryptosQuery(100);
-    const currencyInitialState = cryptosList?.data?.coins;
-    const [cryptos, setCryptos] = useState(null);
-    console.log(cryptos);
-    useEffect(() => {
-        setCryptos(currencyInitialState);
-    }, [currencyInitialState]);
-    const updateCatagory = (c) => {
-        setNewsCategory(c);
-    };
+  const [newsCategory, setNewsCategory] = useState('Cryptocurrency')
+  const { data: cryptoNews } = useGetCryptoNewsQuery({
+    newsCategory,
+    count: simplified ? 3 : 20
+  })
+  console.log(cryptoNews)
+  let { data: cryptosList } = useGetCryptosQuery(100)
+  const currencyInitialState = cryptosList?.data?.coins
+  const [cryptos, setCryptos] = useState(null)
+  console.log(cryptos)
+  useEffect(() => {
+    setCryptos(currencyInitialState)
+  }, [currencyInitialState])
+  const updateCatagory = (c) => {
+    setNewsCategory(c)
+  }
 
-    if (!cryptoNews?.value) return 'Loading...';
-    return (
-        <div className={styles.news}>
-            {!simplified && (
-                <Dropdown
-                    title={newsCategory}
-                    updateCatagory={updateCatagory}
-                    list={currencyInitialState}
-                />
-            )}
-            {cryptoNews?.value.map((news, i) => (
-                <a key={i} href={news.url} target='_blank' rel='noreferrer'>
-                    <NewsCard odd={i % 2 !== 0} news={news} />
-                </a>
-            ))}
-        </div>
-    );
-};
+  if (!cryptoNews?.value) return <LoadingCube />
 
-export default News;
+  return (
+    <div className={styles.news}>
+      {!simplified && (
+        <Dropdown
+          title={newsCategory}
+          updateCatagory={updateCatagory}
+          list={currencyInitialState}
+        />
+      )}
+      {cryptoNews?.value.map((news, i) => (
+        <a key={i} href={news.url} target='_blank' rel='noreferrer'>
+          <NewsCard odd={i % 2 !== 0} news={news} />
+        </a>
+      ))}
+    </div>
+  )
+}
+
+export default News
